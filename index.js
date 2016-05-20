e1ef2611767ecc26949676407133e1384ea0b396
Bala <bala@freshdesk.com>
2016-05-20 16:27:54 +0530

    Some more methods inclusion

--- index.js
+++ index.js
@@ -28,7 +28,7 @@ function createJiraIssue(event, context) {
 function addJiraComment(event, context) {
     console.log('Received event: ', event);
     // verify later the json payload of issue that is needed.
-    jira.issue.createIssue( { issueKey: event.issue_key }, function(error, issue) {
+    jira.issue.addComment( { issueKey: event.issue_key }, function(error, issue) {
         console.log(issue.fields);
         context.done(null, issue)
     });
@@ -37,7 +37,7 @@ function addJiraComment(event, context) {
 function getAllProjects(event, context) {
     console.log('Received event: ', event);
     // verify later the json payload of issue that is needed.
-    jira.issue.createIssue( { issueKey: event.issue_key }, function(error, issue) {
+    jira.issue.getAllProjects( { issueKey: event.issue_key }, function(error, issue) {
         console.log(issue.fields);
         context.done(null, issue)
     });
@@ -46,7 +46,43 @@ function getAllProjects(event, context) {
 function getAllIssueTypes(event, context) {
     console.log('Received event: ', event);
     // verify later the json payload of issue that is needed.
-    jira.issue.createIssue( { issueKey: event.issue_key }, function(error, issue) {
+    jira.issue.getAllIssueTypes( { issueKey: event.issue_key }, function(error, issue) {
+        console.log(issue.fields);
+        context.done(null, issue)
+    });
+}
+
+function getAllFields(event, context) {
+    console.log('Received event: ', event);
+    // verify later the json payload of issue that is needed.
+    jira.field.getAllFields( { issueKey: event.issue_key }, function(error, issue) {
+        console.log(issue.fields);
+        context.done(null, issue)
+    });
+}
+
+function getTransitions(event, context) {
+    console.log('Received event: ', event);
+    // verify later the json payload of issue that is needed.
+    jira.issue.getTransitions( { issueKey: event.issue_key }, function(error, issue) {
+        console.log(issue.fields);
+        context.done(null, issue)
+    });
+}
+
+function deleteIssue(event, context) {
+    console.log('Received event: ', event);
+    // verify later the json payload of issue that is needed.
+    jira.issue.deleteIssue( { issueKey: event.issue_key }, function(error, issue) {
+        console.log(issue.fields);
+        context.done(null, issue)
+    });
+}
+
+function addAttachment(event, context) {
+    console.log('Received event: ', event);
+    // verify later the json payload of issue that is needed.
+    jira.issue.addAttachment( { issueKey: event.issue_key }, function(error, issue) {
         console.log(issue.fields);
         context.done(null, issue)
     });
@@ -58,12 +94,20 @@ exports.handler = function (event, context) {
         fetchJiraIssue(event, context);
     } else if (event.action === 'CreateIssue') {
         createJiraIssue(event, context);
-    } else if (event.action === "addComment") {
+    } else if (event.action === "AddComment") {
         addJiraComment(event, context);
-    } else if (event.action === "allProjects") {
+    } else if (event.action === "FetchAllProjects") {
         getAllProjects(event, context);
-    } else if (event.action === "") {
+    } else if (event.action === "FetchAllIssueTypes") {
         getAllIssueTypes(event, context);
+    } else if (event.action === "GetAllFields") {
+        getAllFields(event, context);
+    } else if (event.action === "GetTransitions") {
+        getTransitions(event, context);
+    } else if (event.action === "DeleteIssue") {
+        deleteIssue(event, context);
+    } else if (event.action === "AddAttachment") {
+        addAttachment(event, context);
     } else {
         context.done(new Error('Action type "' + event.action + '" not supported.'));
     }
