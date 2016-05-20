af3ff19a4213105f078c0304ba39111a038e501d
Murugan <murugu2012@gmail.com>
2016-05-20 19:00:31 +0530

    Adding Working Apis

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 var JiraClient = require('jira-connector');
+var http = require('http');
+var fs = require('fs');
 
 var jira = new JiraClient( {
     host: 'abc.atlassian.net',
@@ -7,84 +9,95 @@ var jira = new JiraClient( {
         password: '----'
     }
 });
-
+// event.data
+//
+// {"issueKey" : 'AIRS-774'}
 function fetchJiraIssue(event, context) {
-    console.log('Received event: ', event);
-    jira.issue.getIssue( { issueKey: event.issue_key }, function(error, issue) {
+    jira.issue.getIssue( { issueKey: event.issueKey }, function(error, issue) {
         console.log(issue.fields);
-        context.done(null, issue)
-    });
-};
-
-function createJiraIssue(event, context) {
-    console.log('Received event: ', event);
-    // verify later the json payload of issue that is needed.
-    jira.issue.createIssue( { issueKey: event.issue_key }, function(error, issue) {
-        console.log(issue.fields);
-        context.done(null, issue)
+        context.done(null, issue);
     });
 }
 
+ // event.data
+ //
+ // {"fields":
+ //  {"project":{"id":"10400"},
+ //   "issuetype":{"id":"10000"},
+ //   "components":[{"id":"10200"}],
+ //   "description":"Bala",
+ //   "security":{"id":"10101"},
+ //   "summary":"This is a sample ticket",
+ //   "priority":{"id":"1"},
+ //   "customfield_10736":[{"id":"10537"}]}}
+function createJiraIssue(event, context) {
+    jira.issue.createIssue( event.data, function(error, issue) {
+        console.log(issue);
+        context.done(null, issue);
+    });
+}
+// event.data
+//
+// { comment: { body: 'Note added by Support' },
+//  issueKey: 'AIRS-774' }
 function addJiraComment(event, context) {
-    console.log('Received event: ', event);
-    // verify later the json payload of issue that is needed.
-    jira.issue.addComment( { issueKey: event.issue_key }, function(error, issue) {
-        console.log(issue.fields);
-        context.done(null, issue)
+    jira.issue.addComment( event.data, function(error, issue) {
+        console.log(issue);
+        context.done(null, issue);
     });
 }
 
 function getAllProjects(event, context) {
-    console.log('Received event: ', event);
-    // verify later the json payload of issue that is needed.
-    jira.issue.getAllProjects( { issueKey: event.issue_key }, function(error, issue) {
-        console.log(issue.fields);
-        context.done(null, issue)
+    jira.project.getAllProjects({}, function(error, project) {
+        console.log(project);
+        context.done(null, project);
     });
 }
 
 function getAllIssueTypes(event, context) {
-    console.log('Received event: ', event);
-    // verify later the json payload of issue that is needed.
-    jira.issue.getAllIssueTypes( { issueKey: event.issue_key }, function(error, issue) {
-        console.log(issue.fields);
-        context.done(null, issue)
+    jira.issueType.getAllIssueTypes({}, function(error, issueType) {
+        console.log(issueType);
+        context.done(null, issueType);
     });
 }
 
 function getAllFields(event, context) {
-    console.log('Received event: ', event);
-    // verify later the json payload of issue that is needed.
-    jira.field.getAllFields( { issueKey: event.issue_key }, function(error, issue) {
-        console.log(issue.fields);
-        context.done(null, issue)
+    jira.field.getAllFields( { }, function(error, field) {
+        console.log(field);
+        context.done(null, field);
     });
 }
-
+// event.data
+//
+// {"issueKey" : 'AIRS-774'}
 function getTransitions(event, context) {
-    console.log('Received event: ', event);
-    // verify later the json payload of issue that is needed.
-    jira.issue.getTransitions( { issueKey: event.issue_key }, function(error, issue) {
-        console.log(issue.fields);
-        context.done(null, issue)
+    jira.issue.getTransitions( { issueKey: event.issueKey }, function(error, issue) {
+        console.log(issue);
+        context.done(null, issue);
     });
 }
-
+// event.data
+//
+// {"issueKey" : 'AIRS-774'}
 function deleteIssue(event, context) {
-    console.log('Received event: ', event);
-    // verify later the json payload of issue that is needed.
-    jira.issue.deleteIssue( { issueKey: event.issue_key }, function(error, issue) {
-        console.log(issue.fields);
-        context.done(null, issue)
+    jira.issue.deleteIssue( { issueKey: event.issueKey }, function(error, message) {
+        console.log(message);
+        context.done(null, message);
     });
 }
-
+// event.data
+//
+// {"issueKey" : 'AIRS-774', 'file': {'url':"http://s3url.com", 'name': 'random.jpg'}}
 function addAttachment(event, context) {
-    console.log('Received event: ', event);
-    // verify later the json payload of issue that is needed.
-    jira.issue.addAttachment( { issueKey: event.issue_key }, function(error, issue) {
-        console.log(issue.fields);
-        context.done(null, issue)
+    var file = fs.createWriteStream(event.file.name);
+    var request = http.get(event.file.url, function(response) {
+        var stream = response.pipe(file);
+        stream.on('finish', function(){
+            jira.issue.addAttachment( { issueKey: event.issueKey, filename: file.path }, function(error, attachment) {
+                console.log(attachment);
+                context.done(null, attachment);
+            });
+        });
     });
 }
 
@@ -109,6 +122,6 @@ exports.handler = function (event, context) {
     } else if (event.action === "AddAttachment") {
         addAttachment(event, context);
     } else {
-        context.done(new Error('Action type "' + event.action + '" not supported.'));
+        context.done(null, ('Action type "' + event.action + '" not supported.'));
     }
 };
